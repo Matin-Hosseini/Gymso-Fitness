8f07592afa7245e6a93e715aaededc00a4ff9c24
Gymso <matinpinterest2003@gmail.com>
2022-09-28 11:59:56 +0330

    Schedules and Table created

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ let navLinks=document.querySelector("#nav-links");
 let hideMenu = document.querySelector("#hide-menu");
 let showMenu = document.querySelector("#show-menu");
 
+showMenu.addEventListener("click", ()=>{
+    navLinks.style.display="block";
+    navLinks.style.right = 0;
+})
+
 hideMenu.addEventListener("click", ()=>{
     navLinks.style.display="none";
 })
-showMenu.addEventListener("click", ()=>{
-    navLinks.style.display="block";
-})
+
 // function HideMenu()
 //  {
   
